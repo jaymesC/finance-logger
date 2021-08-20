c6f6c7b1566d1de66755ec04f2c69058883bb7a0
jaymesC <clementjaymes@gmail.com>
2021-08-20 02:58:32 +0100

    added files to project

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
 "use strict";
 // classes
 var Invoice = /** @class */ (function () {
-    function Invoice(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    // client: string;
+    // details: string;
+    // amount: number;
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     Invoice.prototype.format = function () {
         return this.client + " owes \u00A3" + this.amount + " for " + this.details;
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 // classes
 class Invoice {
-    client: string;
-    details: string;
-    amount: number;
+    // client: string;
+    // details: string;
+    // amount: number;
 
-    constructor(c: string, d: string, a: number){
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number,
+    ){}
 
     format() {
         return `${this.client} owes £${this.amount} for ${this.details}`;
