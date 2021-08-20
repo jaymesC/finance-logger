27c3dec036e34a5a813849a39394b58c330d8428
jaymesC <clementjaymes@gmail.com>
2021-08-20 04:38:03 +0100

    added tuples in app.ts

--- src/app.ts
+++ src/app.ts
@@ -17,11 +17,14 @@ const list = new ListTemplate(ul)
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
 
+    let values: [string, string, number];
+    values = [toFrom.value, details.value, amount.valueAsNumber]
+
     let doc: Hasformatter;
     if (type.value === 'invoice'){
-        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber)
+        doc = new Invoice(...values)
     } else {
-        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber)
+        doc = new Payment(...values)
     }
     
     list.render(doc, type.value, 'end');
